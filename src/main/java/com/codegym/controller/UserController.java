7fa744abcf0e00501c2b01b92a20fd9eb67c20b8
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-09-22 17:14:32 +0700

    crud xong phan trang

--- src/main/java/com/codegym/controller/UserController.java
+++ src/main/java/com/codegym/controller/UserController.java
@@ -5,11 +5,14 @@ import com.codegym.model.User;
 import com.codegym.service.CategoryService;
 import com.codegym.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 
@@ -28,11 +31,24 @@ public class UserController {
 
 
 
+//    @GetMapping("/users")
+//    public ModelAndView listUser() {
+//        Iterable<User> users = userService.findAll();
+//        ModelAndView modelAndView = new ModelAndView("/user/list");
+//        modelAndView.addObject("users",users);
+//        return modelAndView;
+//    }
+
     @GetMapping("/users")
-    public ModelAndView listUser() {
-        Iterable<User> users = userService.findAll();
+    public ModelAndView listCustomers(@RequestParam("s") Optional<String> s, @PageableDefault(value = 3) Pageable pageable){
+        Page<User> users;
+        if(s.isPresent()){
+            users = userService.findAllByusernameContaining(s.get(), pageable);
+        } else {
+            users = userService.findAll(pageable);
+        }
         ModelAndView modelAndView = new ModelAndView("/user/list");
-        modelAndView.addObject("users",users);
+        modelAndView.addObject("users", users);
         return modelAndView;
     }
 
@@ -51,5 +67,48 @@ public class UserController {
         modelAndView.addObject("user", new User());
         modelAndView.addObject("message","New user created successfully");
         return modelAndView;
+    }
+
+    @GetMapping("/edit-user/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        User user = userService.findById(id);
+        if (user != null) {
+            ModelAndView modelAndView = new ModelAndView("/user/edit");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-user")
+    public ModelAndView updateCustomer(@ModelAttribute("user") User user) {
+        userService.save(user);
+        ModelAndView modelAndView = new ModelAndView("/user/edit");
+        modelAndView.addObject("user", user);
+        modelAndView.addObject("message", "User updated successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete-user/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id) {
+        User user = userService.findById(id);
+        if (user != null) {
+            ModelAndView modelAndView = new ModelAndView("/user/delete");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/delete-user")
+    public String deleteCustomer(@ModelAttribute("user") User user) {
+        userService.remove(user.getId());
+        return "redirect:users";
     }
 }
