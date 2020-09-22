df2040c86c7fc998404d546ff1bdda23afd6a15e
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-09-22 15:17:46 +0700

    config build tao bang

---
+++ src/main/java/com/codegym/model/Category.java
@@ -0,0 +1,49 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categorys")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<User> users;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, List<User> users) {
+        this.id = id;
+        this.name = name;
+        this.users = users;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
---
+++ src/main/java/com/codegym/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,6 @@
+package com.codegym.service.impl;
+
+import com.codegym.service.CategoryService;
+
+public class CategoryServiceImpl implements CategoryService {
+}
