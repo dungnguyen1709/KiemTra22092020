d12e3b9ef00dcf28e70126e7987751a505cb3756
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-09-22 17:23:29 +0700

    hoan thien

--- src/main/java/com/codegym/controller/CategoryController.java
+++ src/main/java/com/codegym/controller/CategoryController.java
@@ -23,87 +23,11 @@ public class CategoryController {
     private UserService userService;
 
     @GetMapping("/categorys")
-    public ModelAndView listProvinces(){
+    public ModelAndView listCategory(){
         Iterable<Category> categorys = categoryService.findAll();
         ModelAndView modelAndView = new ModelAndView("/category/list");
         modelAndView.addObject("categotys", categorys);
         return modelAndView;
     }
-
-    @GetMapping("/create-category")
-    public ModelAndView showCreateForm(){
-        ModelAndView modelAndView = new ModelAndView("/category/create");
-        modelAndView.addObject("category", new Category());
-        return modelAndView;
-    }
-
-    @PostMapping("/create-category")
-    public ModelAndView saveProvince(@ModelAttribute("category") Category category){
-        categoryService.save(category);
-
-        ModelAndView modelAndView = new ModelAndView("/category/create");
-        modelAndView.addObject("category", new Category());
-        modelAndView.addObject("message", "New category created successfully");
-        return modelAndView;
-    }
-
-    @GetMapping("/edit-category/{id}")
-    public ModelAndView showEditForm(@PathVariable Long id){
-        Category category = categoryService.findById(id);
-        if(category != null) {
-            ModelAndView modelAndView = new ModelAndView("/category/edit");
-            modelAndView.addObject("category", category);
-            return modelAndView;
-
-        }else {
-            ModelAndView modelAndView = new ModelAndView("/error.404");
-            return modelAndView;
-        }
-    }
-
-    @PostMapping("/edit-category")
-    public ModelAndView updateProvince(@ModelAttribute("category") Category category){
-        categoryService.save(category);
-        ModelAndView modelAndView = new ModelAndView("/category/edit");
-        modelAndView.addObject("category", category);
-        modelAndView.addObject("message", "Category updated successfully");
-        return modelAndView;
-    }
-
-    @GetMapping("/delete-category/{id}")
-    public ModelAndView showDeleteForm(@PathVariable Long id){
-        Category category = categoryService.findById(id);
-        if(category != null) {
-            ModelAndView modelAndView = new ModelAndView("/category/delete");
-            modelAndView.addObject("category", category);
-            return modelAndView;
-
-        }else {
-            ModelAndView modelAndView = new ModelAndView("/error.404");
-            return modelAndView;
-        }
-    }
-
-    @PostMapping("/delete-category")
-    public String deleteProvince(@ModelAttribute("category") Category category){
-        categoryService.remove(category.getId());
-        return "redirect:categorys";
-    }
-
-    @GetMapping("/view-category/{id}")
-    public ModelAndView viewProvince(@PathVariable("id") Long id){
-        Category category = categoryService.findById(id);
-        if(category == null){
-            return new ModelAndView("/error.404");
-        }
-
-        Iterable<User> users = userService.findAllByCategory(category);
-
-        ModelAndView modelAndView = new ModelAndView("/category/view");
-        modelAndView.addObject("category", category);
-        modelAndView.addObject("users", users);
-        return modelAndView;
-    }
-
 
 }
